52ad2afbe0b4774229a4a48f1c66fbf5aeccf91e
Abu Bakker Siddiq <abubakkersiddiqhs@gmail.com>
2020-09-26 23:52:49 +0530

    Updates

---
+++ precache-manifest.0a9f7a7fefddc3844fa1559cb2c0b291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef8085b3562a0ad928c76c8ed14acd44",
+    "url": "/YoutubeVideos/index.html"
+  },
+  {
+    "revision": "41d2ea94fa72a438279e",
+    "url": "/YoutubeVideos/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6f040572e0fc7a7bfba8",
+    "url": "/YoutubeVideos/static/js/2.8ccbfc15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YoutubeVideos/static/js/2.8ccbfc15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d2ea94fa72a438279e",
+    "url": "/YoutubeVideos/static/js/main.6909504e.chunk.js"
+  },
+  {
+    "revision": "8fdf8076b5dfcf77b001",
+    "url": "/YoutubeVideos/static/js/runtime-main.4d02a542.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/YoutubeVideos/static/media/logo.5d5d9eef.svg"
+  }
+]);

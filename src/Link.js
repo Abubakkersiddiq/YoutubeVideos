ccdbfcc9f970c680f3e8d444538c8876744a6d1d
Abu Bakker Siddiq <abubakkersiddiqhs@gmail.com>
2020-09-27 08:41:03 +0530

    api addition

--- src/Link.js
+++ src/Link.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React ,{useState}from 'react';
+import axios from 'axios';
 import './Link.css';
-import { Form, FormGroup, FormInput, FormSelect } from "shards-react";
+import { Form, FormGroup, FormInput, FormSelect, Button } from "shards-react";
 
-function Link() {
+function Link(props) {
+    const [link, setLink] = useState(undefined);
+    const [url, setUrl] = useState('');
+    
+    const handleSearch= async() =>{
+        let linkone = url;
+        let id = linkone.split("=")[1];
+        const responseone = await axios.get(`https://www.googleapis.com/youtube/v3/videos?key=AIzaSyCQPdwWm__RUz9Hhh7GtVmmfYjTciYQxbk&part=snippet,statistics&id=${id}`)
+        setLink(responseone);
+        if(link !== undefined)
+        {
+            if(link.data.items.length !== 0 )
+            {
+                props.onItemUpdate(link);
+            }
+        }
+    }
     return (
         <div>
             <Form>
                 <FormGroup>
                     <label htmlFor="Link" className="Link-input">Enter the Youtube Link</label>
-                    <FormInput id="Link"/>
+                    <FormInput id="Link" onChange={e => setUrl(e.target.value)}/>
                     <label htmlFor="Link" className="Link-input">Choose the Category</label>
                     <FormSelect>
                         <option value="first">Cooking</option>
                         <option value="second">Electrical</option>
                     </FormSelect>
+                    <Button pill theme="primary" className="margin20" onClick={handleSearch}>Search</Button>
                 </FormGroup>
             </Form>
         </div>
--- src/Preview.js
+++ src/Preview.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Preview.css';
 import { Card,
     CardHeader,
     CardTitle,
     CardImg,
     CardBody,
-    CardFooter,
     Button } from "shards-react";
 
-function Preview() {
+function Preview(props) {
+    const [title, settitle] = useState(props.Title);
+    const [cover, setcover] = useState(props.Image);
+
+    useEffect(()=>{
+        setcover(props.Image);
+    },[props.Image]);
+
     return (
         <div className="Preview-notice">
             <Card className="Preview-notice-inner">
                 <CardHeader>Video Information</CardHeader>
-                <CardImg src="https://place-hold.it/300x200" />
+                {cover === null 
+                    ? (<CardImg src="https://place-hold.it/300x200" />)
+                    : (<CardImg src={cover.url} style={{height:"250px"}} />)
+                }
                 <CardBody>
-                    <CardTitle>VIdeo Title</CardTitle>
-                    <Button>Read Description</Button>
+                    <CardTitle>{props.Title}</CardTitle>
                 </CardBody>
                 
             </Card>
